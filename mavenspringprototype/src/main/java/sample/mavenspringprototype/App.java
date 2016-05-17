fa79c56114fdf1e1fd96efc5e8e519bb6e150b88
jugalpanchal <jugalpanchal@Lapi-Jugal-3558>
2016-05-18 00:04:58 +0530

    Added project, class path, POM, Bean.xml, App class and Car class files

---
+++ mavenspringprototype/src/main/java/sample/mavenspringprototype/App.java
@@ -0,0 +1,18 @@
+package sample.mavenspringprototype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		ApplicationContext context = 
+	             new ClassPathXmlApplicationContext("Beans.xml");
+
+	      Car car = (Car) context.getBean("car");
+
+	      String name = car.getName();
+	      System.out.println(name);
+    }
+}
---
+++ mavenspringprototype/src/main/java/sample/mavenspringprototype/Car.java
@@ -0,0 +1,15 @@
+package sample.mavenspringprototype;
+
+public class Car {
+	String name;
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
